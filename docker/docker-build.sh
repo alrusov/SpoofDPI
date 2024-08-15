c8ad5f98adc69218c6112c00b13697e641df6b7f
Alexey Rusov <rolic402@mail.ru>
2024-08-15 19:13:10 +0300

    feat: docker added

---
+++ docker/docker-build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+#----------------------------------------------------------------#
+
+if [ -f .env ]; then
+	. .env
+fi
+
+#----------------------------------------------------------------#
+
+BUILD_HOME=___DOCKER_TMP___
+
+APP=spoof-dpi
+APP_HOME=/opt/${APP}
+
+TAG_LATEST="${APP}:${VERSION:=latest}"
+
+rm -rf ${BUILD_HOME}
+mkdir -p ${BUILD_HOME}/
+cp -r ../spoof-dpi ${BUILD_HOME}/
+
+docker build \
+	-t ${TAG_LATEST} \
+	--build-arg BUILD_HOME=${BUILD_HOME} \
+	--build-arg APP_HOME=${APP_HOME} \
+	--build-arg APP=${APP} \
+	--build-arg PORT=${PORT} \
+	--build-arg DOCKER_USER=${DOCKER_USER} \
+	--build-arg DOCKER_UID=${DOCKER_UID} \
+	--build-arg DOCKER_GROUP=${DOCKER_GROUP} \
+	--build-arg DOCKER_GID=${DOCKER_GID} \
+	.
+
+rm -rf ${BUILD_HOME}
+
+#----------------------------------------------------------------#
+
+#if [ "$1" == "push" -o "$1" == "" ]; then
+#	docker push ${TAG_LATEST}
+#fi
+
+#----------------------------------------------------------------#
